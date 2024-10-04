df892e23307f28d25a32958a027b1161af62fe64
AceDemo1 <lawal641@gmail.com>
2024-10-04 11:48:38 +0000

    get budget

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,1 @@
-export default function createIteratorObject(report) {
-  const myarray = [];
-  for (const item of Object.values(report.allEmployees)) {
-    myarray.push(...item);
-  }
 
-  return myarray;
-}
