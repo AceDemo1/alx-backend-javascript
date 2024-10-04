9dad95f704685b38a347215d7789ce6b779c8f4d
AceDemo1 <lawal641@gmail.com>
2024-10-04 15:10:21 +0000

    for_of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,10 @@
 /* eslint-disable guard-for-in */
 /* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-  const myarray = [];
   for (const item of array) {
-    myarray.push(appendString + item);
+    const i = array.indexOf(item);
+    array[i]: (appendString + item);
   }
 
-  return myarray;
+  return array;
 }
